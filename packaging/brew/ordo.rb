2da9a8f489050b32090f09c956ff27d4683ef34f
Elwardi <e.fadeli@ianus-simulation.de>
2026-08-31 09:37:56 +0200

    chore: bump version to 0.2.0

--- packaging/brew/ordo.rb
+++ packaging/brew/ordo.rb
@@ -2,16 +2,16 @@
 class Ordo < Formula
   desc "Comprehension-optimized ordering of code-change hunks"
   homepage "https://github.com/elwardi/ordo"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/elwardi/ordo/releases/download/v0.1.0/ordo-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/elwardi/ordo/releases/download/v0.2.0/ordo-aarch64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/elwardi/ordo/releases/download/v0.1.0/ordo-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/elwardi/ordo/releases/download/v0.2.0/ordo-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_X86_64_SHA256"
     end
   end
